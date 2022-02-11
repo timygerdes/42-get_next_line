801d7f414208486f3a904e9e72aab27343848232
Tim <tgerdes@student.42wolfsburg.de>
2022-02-11 13:25:19 +0100

    Initialize repo

---
+++ get_next_line.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tgerdes <tgerdes@student.42wolfsburg.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/22 23:20:08 by tgerdes           #+#    #+#             */
+/*   Updated: 2021/10/15 18:08:51 by tgerdes          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 4096
+# endif
+
+# ifndef MAX_FD
+#  define MAX_FD 1024
+# endif
+
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	slen(const char *s);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+void	*ft_malloc(size_t count, size_t size);
+char	*prepare_string(int fd, int bytes_read, char *rtn_str, char **tmpstr);
+
+#endif
---
+++ get_next_line_utils.c
@@ -0,0 +1,100 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tgerdes <tgerdes@student.42wolfsburg.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/22 23:20:10 by tgerdes           #+#    #+#             */
+/*   Updated: 2021/10/15 18:26:56 by tgerdes          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	slen(const char *s)
+{
+	size_t	c;
+
+	c = 0;
+	if (!s)
+		return (0);
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+	return (c);
+}
+
+void	ft_bzero(void *s, size_t n)
+{
+	char	*pchar1;
+
+	pchar1 = s;
+	while (n > 0)
+	{
+		*pchar1 = '\0';
+		n--;
+		pchar1++;
+	}
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	if (!s)
+		return (NULL);
+	while (s[i] != '\0')
+	{
+		if (s[i] == (char)c)
+			return ((char *)(&s[i]));
+		i++;
+	}
+	if (s[i] == (char)c)
+		return ((char *)(&s[i]));
+	return (NULL);
+}
+
+void	*ft_malloc(size_t count, size_t size)
+{
+	void			*rtn_str;
+	unsigned char	*tmp_str;
+
+	rtn_str = malloc(count * size);
+	if (!rtn_str)
+		return (NULL);
+	tmp_str = rtn_str;
+	*tmp_str = '\0';
+	return (rtn_str);
+}
+
+char	*prepare_string(int fd, int bytes_read, char *rtn_str, char **tmpstr)
+{
+	static char	buf[MAX_FD][BUFFER_SIZE + 1];
+
+	while (!ft_strchr(buf[fd], '\n'))
+	{
+		if (*buf[fd])
+			rtn_str = ft_strjoin(rtn_str, buf[fd]);
+		bytes_read = read(fd, buf[fd], BUFFER_SIZE);
+		buf[fd][bytes_read] = '\0';
+		if (bytes_read <= 0 && !(*rtn_str))
+		{
+			free(rtn_str);
+			return (NULL);
+		}
+		if (bytes_read < BUFFER_SIZE && !ft_strchr(buf[fd], '\n'))
+		{
+			rtn_str = ft_strjoin(rtn_str, buf[fd]);
+			ft_bzero(buf[fd], 1);
+			return (rtn_str);
+		}
+	}
+	*tmpstr = ft_strjoin(rtn_str, buf[fd]);
+	rtn_str = ft_substr(*tmpstr, 0, ((ft_strchr(*tmpstr, '\n') - *tmpstr + 1)));
+	ft_memmove(buf[fd], *tmpstr + slen(rtn_str), slen(*tmpstr) - slen(rtn_str));
+	ft_bzero(&buf[fd][slen(*tmpstr) - slen(rtn_str)], 1);
+	return (rtn_str);
+}
